ae989b651618a19f154c71abd8767f05c9ea51c4
JaviAzagra <javiazagra12@gmail.com>
2022-10-10 18:33:07 +0200

    Iteration#6

---
+++ 6_Funcion-swap.js
@@ -0,0 +1,4 @@
+/* Crea una función llamada swap() que reciba un array y dos parametros que sean indices del array. 
+La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. 
+Retorna el array resultante. */
+
